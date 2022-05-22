6c5fd76c94e9b6d3ac396d4f68d1c9ddf91cf971
Марина Ефимова <marina.efimova.00@bk.ru>
2022-05-22 17:41:41 +0400

    Добавьте файлы проекта.

---
+++ AIS_LinkedList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AIS_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+            // добавление элементов
+            linkedList.Add("Tom");
+            linkedList.Add("Alice");
+            linkedList.Add("Bob");
+            linkedList.Add("Sam");
+
+            // выводим элементы
+            foreach (var item in linkedList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Write("\n ");
+            // удаляем элемент
+            linkedList.Remove("Alice");
+            foreach (var item in linkedList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Write("\n ");
+            // проверяем наличие элемента
+            bool isPresent = linkedList.Contains("Sam");
+            Console.WriteLine(isPresent == true ? "Sam присутствует" : "Sam отсутствует");
+
+            // добавляем элемент в начало            
+            linkedList.AppendFirst("Bill");
+
+            // выводим элементы
+            foreach (var item in linkedList)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ DoublylLinkedList/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace DoublylLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<string> linkedList = new DoublyLinkedList<string>();
+            // добавление элементов
+            linkedList.Add("Bob");
+            linkedList.Add("Bill");
+            linkedList.Add("Tom");
+            linkedList.AddFirst("Kate");
+            foreach (var item in linkedList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Write("\n ");
+            // удаление
+            linkedList.Remove("Bill");
+
+            foreach (var item in linkedList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Write("\n ");
+            // перебор с последнего элемента
+            foreach (var t in linkedList.BackEnumerator())
+            {
+                Console.Write(t + " ");
+            }
+        }
+    }
+}
